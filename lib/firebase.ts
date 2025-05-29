73a521490b4b35fb936e41a7a6bfdd77fef9419c
yassinejebbouri <117610079+yassinejebbouri@users.noreply.github.com>
2025-05-30 00:49:48 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth"
 // Your web app's Firebase configuration
 // Replace these values with your Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCJFMlQ50_3lln_pAs3rpVnS-0O0yfG9tU",
-  authDomain: "career-path-navigator-5e4b6.firebaseapp.com",
-  projectId: "career-path-navigator-5e4b6",
-  storageBucket: "career-path-navigator-5e4b6.firebasestorage.app",
-  messagingSenderId: "561316810375",
-  appId: "1:561316810375:web:9f0ac4252ae76e1f493222",
-  measurementId: "G-W0WCPFWM4M"
+  apiKey: ,
+  authDomain: ,
+  projectId: ,
+  storageBucket: ,
+  messagingSenderId: ,
+  appId: ,
+  measurementId: 
 };
 
 // Initialize Firebase
