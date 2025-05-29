f407e9ef0e96e834c8aa0c7204d5666d4e3de083
yassinejebbouri <117610079+yassinejebbouri@users.noreply.github.com>
2025-05-30 00:50:17 +0100

    Update neo4j.ts

--- lib/neo4j.ts
+++ lib/neo4j.ts
@@ -5,9 +5,9 @@ let driver: Driver | null = null
 export function initNeo4j() {
   try {
     // Replace with your Neo4j connection details
-    const uri = process.env.NEO4J_URI || "neo4j://localhost:7690"
-    const user = process.env.NEO4J_USER || "neo4j"
-    const password = process.env.NEO4J_PASSWORD || "yyyyyyyy"
+    const uri = process.env.NEO4J_URI
+    const user = process.env.NEO4J_USER 
+    const password = process.env.NEO4J_PASSWORD 
 
     driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
 
